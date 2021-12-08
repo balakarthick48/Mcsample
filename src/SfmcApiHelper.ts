a53753d1b82055a55ccce899db3fec9481e71789
balakarthick48 <93905639+balakarthick48@users.noreply.github.com>
2021-12-07 17:43:01 -0800

    Update SfmcApiHelper.ts

--- src/SfmcApiHelper.ts
+++ src/SfmcApiHelper.ts
@@ -77,7 +77,7 @@ export default class SfmcApiHelper
                 let accessToken = response.data.accessToken;
                 let tokenExpiry = new Date();
                 tokenExpiry.setSeconds(tokenExpiry.getSeconds() + response.data.expiresIn);
-                Utils.logInfo("Got OAuth token: " + (accessToken) + ", expires = " +  tokenExpiry);
+                Utils.logInfo("Got OAuth token: " + accessToken + ", expires = " +  tokenExpiry);
 
                 resolve(
                 {
